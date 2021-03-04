0ddff941bfcc9bd9e6dd3ad74739b963734dff9f
Aji Suryawan <ajisuryawan@smkrus.sch.id>
2021-03-04 08:11:54 +0700

    cardview added

--- app/src/main/java/com/example/practice10rplall/DashboardMenu.java
+++ app/src/main/java/com/example/practice10rplall/DashboardMenu.java
@@ -3,12 +3,26 @@ package com.example.practice10rplall;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class DashboardMenu extends AppCompatActivity {
+    // Bundle : satu set mekanisme untuk mengambil nilai yang dikirimkan pada Class DashboardMenu
+    Bundle extras;
+    String email;
+    TextView tvemail;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard_menu);
+        tvemail = (TextView)findViewById(R.id.tvemail);
+        extras = getIntent().getExtras();
+        if (extras != null) {
+            email = extras.getString("email");
+            tvemail.setText(email);
+        }
+
+
+
     }
 }
--- app/src/main/java/com/example/practice10rplall/MainActivity.java
+++ app/src/main/java/com/example/practice10rplall/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.practice10rplall;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -33,6 +34,12 @@ public class MainActivity extends AppCompatActivity {
                         && password.equalsIgnoreCase("123456")){
                     //sukses login, Toast
                     Toast.makeText(MainActivity.this, "Sukses Login", Toast.LENGTH_SHORT).show();
+                    // pindah ke tampilan lain / activity
+                    Intent move = new Intent(MainActivity.this , DashboardMenu.class);
+                    // nambahin data -> paketan
+                    // putextra tidak hanya satu
+                    move.putExtra("email" , email);
+                    startActivity(move);
                 }else {
                     Toast.makeText(MainActivity.this, "Gagal Login gan !", Toast.LENGTH_SHORT).show();
                 }
